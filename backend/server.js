d0486cc0d061cebbdd4b1fa9b49573b82dc93f31
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2026-01-30 11:38:00 -0500

    feat: integrated MongoDB connecton into Express server

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Middleware
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-// Test route
 app.get('/', (req, res) => {
   res.send('Backend server is running');
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server started');
 });
