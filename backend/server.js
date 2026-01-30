88390dfc60f9d4c222dc61cf3c2c41346d7b53b7
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2026-01-30 15:17:52 -0500

    feat: register auth routes and add JWT secret to environment config

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
 
 const app = express();
 
