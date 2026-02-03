b7b7f377a19f71101bf4c19fcc0e796116a5a97d
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2026-02-03 15:44:45 -0500

    feat(api): aded user and admin ticket routes with clear separation

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -6,31 +6,21 @@ const {
   createTicket,
   getMyTickets,
   getTicketById,
+  deleteMyTicket,
   getAllTicketsAdmin,
   updateTicketStatusAdmin,
   deleteTicketAdmin,
 } = require("../controllers/ticketController");
 
-const commentRoutes = require("./commentRoutes");
-
-// sanity check
-router.get("/ping", (req, res) => {
-  res.json({ message: "tickets route mounted" });
-});
-
-// user ticket routes
+// user
 router.post("/", protect, createTicket);
 router.get("/", protect, getMyTickets);
+router.get("/:id", protect, getTicketById);
+router.delete("/:id", protect, deleteMyTicket);
 
-// admin routes BEFORE :id
+// admin
 router.get("/admin/all", protect, adminOnly, getAllTicketsAdmin);
 router.patch("/admin/:id/status", protect, adminOnly, updateTicketStatusAdmin);
 router.delete("/admin/:id", protect, adminOnly, deleteTicketAdmin);
-
-// nested comment routes
-router.use("/:ticketId/comments", commentRoutes);
-
-// MUST COME LAST
-router.get("/:id", protect, getTicketById);
 
 module.exports = router;
