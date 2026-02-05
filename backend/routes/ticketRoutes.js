d79845ceff8ad602ec90e8f1dce98875998740f8
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2026-02-05 11:16:15 -0500

    fix: bugs

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const { protect, admin } = require("../middleware/authMiddleware");
 const {
   createTicket,
   getAllTicketsAdmin,
   deleteTicket,
 } = require("../controllers/ticketController");
 
-const { protect, admin } = require("../middleware/authMiddleware");
-
-// Create ticket (user)
 router.post("/", protect, createTicket);
-
-// Admin routes
 router.get("/admin/all", protect, admin, getAllTicketsAdmin);
 router.delete("/:id", protect, admin, deleteTicket);
 
